a3a901120387a94f9a7a1f964276b7187150534d
Alice Rixte <alice.rixte@gmail.com>
2016-10-07 10:31:17 +0200

    test pour la sim:plification des formules

--- main.ml
+++ main.ml
@@ -1,8 +1,35 @@
 open Formula
 open Graph
 
+let f = AX(
+	    Equiv(
+		EU(
+		    AG(
+			Prop("a")
+		      ),
+		    AU(
+			EF(Prop("b")),Prop("c")
+		      )
+		  ),
+		Or(
+		    Not(
+			EG(
+			    EX(
+				And(
+				    True,False
+				  )
+			      )
+			  )
+		      ),
+		    AF(
+			Imply(Prop("a"),Prop("b"))
+		      )
+		  )
+	      ))
+
 let main() = 
-  Printf.printf "%s\n" "*****************   Model-checker de formules CTL  ********************"
+  Printf.printf "%s\n" "*****************   Model-checker de formules CTL  ********************";
+  ignore (simplify_formula f)
 
 let () = main()
   
